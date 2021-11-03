e9b0d9bc2faa2d45e34e76f05f92e7cb6612270e
IvaninNikita <nikita.ivanin@bostongene.com>
2021-11-03 18:41:06 +0300

    First homework for Ant series

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import './App.css';
+import React from 'react';
+import { Layout, Menu} from 'antd';
+import {Link, Route, Switch, withRouter} from "react-router-dom";
+import {MainPage} from "./Components/MainPage";
+import {PageAbout} from "./Components/PageAbout";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+const {Header, Content, Footer} = Layout
+
+class App extends React.Component{
+    render() {
+        let path = this.props.location.pathname;
+        let selectedKey;
+        if (path === '/') {
+            selectedKey = ['1'];
+        } else if (path === '/about') {
+            selectedKey = ['2'];
+        } else {
+            throw new Error("Unexpected path!")
+        }
+
+        return (
+            <Layout>
+                <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={selectedKey}>
+                        <Menu.Item key="1">
+                            <Link to='/'>Главная</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to='/about'>О нас</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+                <Content>
+                    <div className="main-container">
+                        <Switch>
+                            <Route path='/about'>
+                                <PageAbout/>
+                            </Route>
+                            <Route path='/'>
+                                <MainPage/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>Nikita Ivanin from MIPT ©2021 Created by Ant UED</Footer>
+            </Layout>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Typography} from "antd";
+
+export class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <Typography.Title>
+                    Добро пожаловать!
+                </Typography.Title>
+
+                <Typography.Paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus imperdiet vehicula. Nam id mauris lorem. Etiam elit tellus, eleifend ut sollicitudin nec, pharetra id arcu. Nam varius lacus sagittis sodales pharetra. Pellentesque commodo orci eget nisl tempor pellentesque at at urna. Sed condimentum auctor leo ac aliquet. Quisque in tortor et metus vestibulum faucibus vel a diam. Nulla efficitur, arcu vel aliquet rhoncus, erat arcu laoreet lectus, at facilisis orci nunc in leo. Nulla facilisi.
+                </Typography.Paragraph>
+                <Typography.Paragraph>
+                    Suspendisse nulla lacus, rhoncus sed magna lobortis, dictum tincidunt ante. Donec imperdiet arcu a arcu ullamcorper, ac pretium odio tempus. Nunc a hendrerit est, ac condimentum quam. Proin sollicitudin justo sit amet libero euismod, non semper mauris consectetur. Nulla sollicitudin felis justo, sit amet hendrerit nunc elementum at. Nulla aliquet urna sed ex suscipit, eu maximus neque tristique. Donec bibendum, magna in ornare convallis, leo ex sollicitudin risus, nec euismod nisi nulla vitae dolor. Phasellus fringilla mauris a massa tincidunt, vel sollicitudin elit molestie.
+                </Typography.Paragraph>
+                <Typography.Paragraph>
+                    Donec quis fringilla tortor, pharetra vehicula ligula. Mauris cursus sem in lacinia fermentum. Nullam faucibus tortor sit amet luctus facilisis. Nam vel tellus a erat tempus malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lorem elit, tempus sit amet ipsum non, auctor blandit odio. Morbi pretium fringilla feugiat. Fusce eu ex ut magna blandit maximus.
+                </Typography.Paragraph>
+
+                <Typography.Title level={2}>
+                    О Лондоне
+                </Typography.Title>
+
+                <Typography.Paragraph>
+                    Fusce nec gravida felis, sed semper diam. Nulla aliquam dui nisi, quis sagittis lectus venenatis nec. Nunc eu purus eleifend, feugiat mi ac, finibus urna. Ut nec tellus iaculis, tristique quam eget, efficitur dui. Fusce ac ex id mi consequat ullamcorper eget id est. Cras suscipit dictum risus, eu venenatis elit ultricies sit amet. Donec dapibus elit in faucibus varius. Vestibulum eget sodales neque, sit amet varius quam. Aliquam ac lacus nisl. Fusce pellentesque at velit eu imperdiet. Phasellus at tristique lorem, tempus ultrices erat. Maecenas vel tempor felis. Pellentesque aliquet enim ut lacinia lobortis. Praesent tincidunt et sapien sit amet iaculis. Donec pharetra fringilla justo vitae fermentum. Phasellus ornare massa vitae mi pretium, ullamcorper sollicitudin libero semper.
+                </Typography.Paragraph>
+                <Typography.Paragraph>
+                    Sed facilisis neque purus, ac fringilla lacus auctor non. Proin a vehicula mi. Praesent fringilla dignissim elit vitae imperdiet. Morbi feugiat laoreet elit vitae facilisis. Pellentesque facilisis eu justo ac sollicitudin. In congue tristique sapien, quis tincidunt nisi fringilla sit amet. Vestibulum rhoncus aliquet lacus, id sodales erat hendrerit in. Etiam tincidunt eu lectus et elementum. Nunc maximus blandit mattis. Praesent dui augue, consequat ut condimentum ut, viverra vel erat. Curabitur quis pretium magna, ut accumsan ipsum. Vivamus pharetra tempor porta. Mauris at nunc luctus ex cursus malesuada.
+                </Typography.Paragraph>
+            </div>
+        );
+    }
+}
