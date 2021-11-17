42dca5063165caf5ff9a6fc612b41ebdc54ffe08
IvaninNikita <nikita.ivanin@bostongene.com>
2021-11-17 13:09:20 +0300

    Added materials for Third HW in Ant series

--- src/Components/PageAbout.jsx
+++ src/Components/PageAbout.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Octokit} from "@octokit/core";
 import {Typography} from "antd";
 
-export class PageAbout extends React.Component {
-    render() {
-        return (
-            <div>
-                <Typography.Title>
-                    О нас!
-                </Typography.Title>
-            </div>
-        );
-    }
+const octokit = new Octokit({auth: `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`});
+
+function useGithubData(username) {
+    const [data, setData] = useState({data: {}});
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await octokit.request('GET /users/{username}', {
+                username: username
+            });
+            setData(response)
+        };
+        getData();
+    }, [username]);
+
+    return data;
+}
+
+
+export function PageAbout() {
+    const githubProfileData = useGithubData('wordedword75049');
+    return (
+        <div>
+            <Typography.Title>
+                О разработчике:
+            </Typography.Title>
+            <Typography.Paragraph>
+                Имя: {githubProfileData.data.name}
+            </Typography.Paragraph>
+            <Typography.Paragraph>
+                Био: {githubProfileData.data.bio}
+            </Typography.Paragraph>
+        </div>
+    );
 }
